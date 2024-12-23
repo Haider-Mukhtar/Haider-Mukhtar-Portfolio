ad9e4370eebfe1d71a8eb1cd7c66de49f346d211
Haider Mukhtar <haidermukhtarch405@gmail.com>
2024-12-23 16:17:47 +0500

    Haider-Mukhtar-Portfolio v-0.1.6

--- src/components/About/Experience.jsx
+++ src/components/About/Experience.jsx
@@ -41,7 +41,7 @@ const experience = [
 
 function Experience() {
     return (
-        <div className='flex justify-center xl:mt-8'>
+        <div className='flex justify-center xl:mt-8 mt-10'>
             <div className='w-4/5 xl:px-14 lg:px-8'>
                 <div className='text-5xl sm:text-5xl font-semibold font-serif text-center'>
                     Work Experience
